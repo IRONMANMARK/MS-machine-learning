349f10e9c54ec401f0bbb8ef3cbe8be19f611df9
IRONMANMARK <33693731+IRONMANMARK@users.noreply.github.com>
2019-05-16 11:41:30 -0400

    Add files via upload

--- cnn.py
+++ cnn.py
@@ -1,3 +1,8 @@
+
+'''
+This is the main script for CNN
+Author: Zichen Liu
+'''
 import sqlite3
 from tqdm import tqdm
 import numpy as np
@@ -7,6 +12,12 @@ from keras.layers import Dense, Conv1D, Flatten, MaxPooling1D, GlobalAveragePool
 from keras.optimizers import SGD
 
 def get_amino_dic(database):
+    '''
+    This is the function will get the aminoacid dic
+    This function will not be called in this script
+    :param database: The database you want to generated this dic
+    :return: return the dic
+    '''
     db = sqlite3.connect(database)
     aminoacid = list(db.execute("select * from amino_acid"))
     amino_dic = dict()
@@ -14,34 +25,18 @@ def get_amino_dic(database):
         amino_dic[aminoacid[i][0]] = i+1
     return amino_dic
 
-def get_amino_result_matrix(dic, database2):
-    db2 = sqlite3.connect(database2)
-    cur2 = db2.cursor()
-    seq_list = list(db2.execute("select * from filter_seq"))
-    result_list =[]
-    long = []
-    for i in range(len(seq_list)):
-        one_result = []
-        a = seq_list[i][0]
-        long.append(len(seq_list[i][0]))
-        for ii in range(len(a)):
-            one_result.append(dic.get(a[ii]))
-        result_list.append(one_result)
-    print( min(long), max(long))
-    long2 = []
-    each = []
-    for i in tqdm(range(len(seq_list))):
-        data_list = list(db2.execute("select pepmass, charge, m_z, intensity from mgfData where seq = ?", (seq_list[i])))
-        long2.append(len(data_list))
-        each.append(len(data_list) / len(seq_list[i][0]))
-    print(min(long2), max(long2), sum(long2), len(long2))
-    print(min(each), max(each))
-    return 0
+
 
 def get_deco_list(database):
+    '''
+    This is the function will get decoration sequence parsed into individual number
+    :param database: the database you want to generated this set
+    :return: return the set of individual decoration sequence number
+    '''
     db = sqlite3.connect(database)
     deco_list = list(db.execute("select * from seq_deco"))
     a_set = set()
+    # parse the decoration sequence to individual number
     for deco in deco_list:
         a = str(deco[0])
         a_list = []
@@ -64,6 +59,12 @@ def get_deco_list(database):
     return a_set
 
 def deco_cnn_result_list(database):
+    '''
+    This is the main function for training CNN and write the result to a file
+    :param database: the database you want to get data from
+    :return: no return
+    '''
+    # get the result label list
     deco_list = sorted(list(get_deco_list(database)))
     db2 = sqlite3.connect(database)
     seq_list = list(db2.execute("select * from filter_seq"))
@@ -74,33 +75,21 @@ def deco_cnn_result_list(database):
     x_fit = []
     y_fit = []
     file2 = open('result.txt', 'w')
+    # create CNN model
     model = Sequential()
 
-    model.add(Convolution1D(nb_filter=256, filter_length=1, input_shape=(4684, 1)))
+    model.add(Convolution1D(nb_filter=128, filter_length=1, input_shape=(4684, 1)))
     model.add(Activation('relu'))
     model.add(Flatten())
     model.add(Dropout(0.4))
-    model.add(Dense(1024, activation='relu'))
     model.add(Dense(512, activation='relu'))
+    model.add(Dense(256, activation='relu'))
     model.add(Dense(7))
     model.add(Activation('softmax'))
     sgd = SGD(lr=0.01, nesterov=True, decay=1e-6, momentum=0.9)
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
     model.summary()
-    # model = Sequential()
-    # model.add(Conv1D(512, 3, input_shape=(4684, 1)))
-    # model.add(Activation('relu'))
-    # model.add(Dropout(0.4))
-    # model.add(Flatten())
-    # model.add(Dense(2048, activation='relu'))
-    # model.add(Dense(1024, activation='relu'))
-    # model.add(Dense(7))
-    # model.add(Activation('softmax'))
-    # sgd = SGD(lr=0.01, nesterov=True, decay=1e-6, momentum=0.9)
-    # model.compile(loss='binary_crossentropy',
-    #               optimizer=sgd,
-    #               metrics=['accuracy'])
-    # print(model.summary())
+    # get data from the database and train it.
     for i in range(len(seq_list)):
         count = i
         data_list = list(db2.execute("select pepmass, score, charge, m_z, intensity from random where seq = ?", (seq_list[i])))
@@ -109,6 +98,7 @@ def deco_cnn_result_list(database):
         a_list = []
         num_list = []
         a_final = []
+        # parse the data and put data in the training matrix and result matrix
         for i in a:
             a_list.append(i)
         for i in range(len(a_list)):
@@ -135,11 +125,11 @@ def deco_cnn_result_list(database):
             # model.fit(x_train2, y_train2, epochs=15, verbose=1)
             print(np.array([np.expand_dims(np.array(data_mz), axis=1)]).shape)
             print(np.expand_dims(np.array(on_hot_sub), axis=0).shape)
-            # break
+            # train the matrix
             model.fit(np.array([np.expand_dims(np.array(data_mz), axis=1)]),
                       np.expand_dims(np.array(on_hot_sub), axis=0), epochs=15, verbose=1)
-            # print(np.expand_dims(np.array(data_mz), axis=1).shape)
         else:
+            # this is the 10% for testing the data
             on_hot_sub = [0, 0, 0, 0, 0, 0, 0]
             for i in range(len(b)):
                 on_hot_sub[deco_list.index(b[i])] = 1
@@ -152,49 +142,7 @@ def deco_cnn_result_list(database):
             result = model.evaluate(np.array([np.expand_dims(np.array(data_mz), axis=1)]),
                       np.expand_dims(np.array(on_hot_sub), axis=0))
             file2.write(result)
-        # print(data_list)
-    x_train = np.array(x_train)
-    y_train = np.array(y_train)
-    x_fit = np.array(x_fit)
-    y_fit = np.array(y_fit)
-    # print(x_train.shape)
-    # CNN_core(x_train, y_train, x_fit, y_fit, 4684)
-
-def CNN_core(x_train, y_train, x_fit, y_fit, seq_length, epochs=15):
-    model = Sequential()
-    model.add(Conv1D(64, 3, activation='relu', input_shape=(seq_length, 1)))
-    model.add(Conv1D(64, 3, activation='relu'))
-    model.add(MaxPooling1D(3))
-    model.add(Conv1D(128, 3, activation='relu'))
-    model.add(Conv1D(128, 3, activation='relu'))
-    model.add(GlobalAveragePooling1D())
-    model.add(Dropout(0.5))
-    model.add(Dense(1, activation='sigmoid'))
-    sgd = SGD(lr=0.01, nesterov=True, decay=1e-6, momentum=0.9)
-    model.compile(loss='binary_crossentropy',
-                  optimizer=sgd,
-                  metrics=['accuracy'])
-    print(model.summary())
-    model.fit(x_train, y_train, epochs=epochs, verbose=1)
-    result = model.evaluate(x_fit, y_fit)
-    print(result)
-    # model.fit(x_train, y_train, batch_size=16, epochs=10)
-    # score = model.evaluate(x_test, y_test, batch_size=16)
-
-
 
 
 if __name__ == "__main__":
     print(deco_cnn_result_list('test4.db'))
-    # a = np.array([1,2,3,4])
-    # print(np.expand_dims(a, axis=1))
-    # CNN_core(4684)
-    # get_amino_result_matrix(get_amino_dic('test.db'), 'test.db')
-    # a = get_amino_dic('test.db')
-    # print(a)
-    # print(get_result_matrix(a, 'test.db'))
-    # db = sqlite3.connect("test3.db")
-    # db.execute("create index seq_index on random (seq)")
-    # db.close()
-    # db = sqlite3.connect('test.db')
-    # db.execute("create index seq_index on mgfData (seq)")
--- fetch_data.py
+++ fetch_data.py
@@ -1,20 +1,28 @@
+
+'''
+This is the file that will fetch data from the database and create unique peptide sequence
+ and peptide decoration sequence index
+ Author: Zichen Liu
+'''
 import sqlite3
 import datetime
 import re
 import tqdm
-from ctypes import *
 import multiprocessing
 import random
-from multiprocessing import Process
-from threading import Thread
 
 
-start = datetime.datetime.now()
-database = 'test.db'
-database2 = 'test3.db'
-def generate_seq_list(database):
+def generate_seq_list(database, table_name):
+    '''
+    This is the function that will generate the unique peptide sequence index table.
+    :param database: The database that you want to have a peptide sequence index table form like "test.db"
+    :param table_name: The table name for the table that contain the raw data.
+                        (Due to the table name I named for different database, This variable is required)
+    :return: return a message said "Generation completed!"
+    '''
     db = sqlite3.connect(database)
     cur = db.cursor()
+    # create a table that will contain the unique peptide sequence
     try:
         cur.execute('DROP TABLE filter_seq')
         # db.execute('VACUUM')
@@ -33,18 +41,24 @@ def generate_seq_list(database):
             print('Database has been successfully created!')
         except:
             print('Database has already been created!')
-
-    db.execute("insert into filter_seq select seq from random group by seq having count (*) > 2 order by seq")
-    # print(seq_tuple)
-    # db.executemany("insert into filter_seq values (?)", (seq_list))
+    # This is call for SQL that will filter out the unique sequence and put it in the newly created table
+    db.execute("insert into filter_seq select seq from %s group by seq having count (*) > 2 order by seq" % table_name)
     db.commit()
     db.close()
     suess = 'Generation complete ! '
     return suess
 
 def generate_seq_decorate_list(database, table_name):
+    '''
+    This is the function that will generate the unique peptide decoration sequence index table.
+    :param database: The database that you want to have a peptide sequence index table form like "test.db"
+    :param table_name: The table name for the table that contain the raw data.
+                        (Due to the table name I named for different database, This variable is required)
+    :return: return a message said "Generation completed!"
+    '''
     db = sqlite3.connect(database)
     cur = db.cursor()
+    # This part will generate a table that will contain the peptide decoration sequence.
     try:
         cur.execute('DROP TABLE seq_deco')
         # db.execute('VACUUM')
@@ -63,10 +77,10 @@ def generate_seq_decorate_list(database, table_name):
             print('Database has been successfully created!')
         except:
             print('Database has already been created!')
-
-    db.execute("insert into seq_deco select seq_decorate from %s group by seq_decorate having count (*) > 2 order by seq_decorate" %table_name)
-    # print(seq_tuple)
-    # db.executemany("insert into filter_seq values (?)", (seq_list))
+    # This is call for SQL that will filter out the unique peptide decoration sequence
+    # and put it in the newly created table
+    db.execute("insert into seq_deco select seq_decorate from %s"
+               " group by seq_decorate having count (*) > 2 order by seq_decorate" %table_name)
     db.commit()
     db.close()
     suess = 'Generation complete! '
@@ -74,8 +88,15 @@ def generate_seq_decorate_list(database, table_name):
 
 
 def purge_numerical_data(database):
+    '''
+    This is the function that will purge the numerical data. mainly will seperate the number in the decoration sequence
+    This function will not be used in this script
+    :param database: The database you want to purge the numerical data
+    :return: Return a message said "Done!"
+    '''
     db = sqlite3.connect(database)
     cur = db.cursor()
+    # This will create 2 tables that will be used in the code
     try:
         cur.execute('DROP TABLE filter_seq_deco2')
         cur.execute('DROP TABLE filter_seq_deco3')
@@ -101,9 +122,11 @@ def purge_numerical_data(database):
             print('Database has been successfully created!')
         except:
             print('Database has already been created!')
+    # get the whole list of the decoration sequence
     a = list(db.execute("select * from filter_seq_deco"))
     c = []
     b = []
+    # parse the decoration in to number
     for i in range(len(a)):
         p = a[i][0].strip('-')
         aa = '-' in p
@@ -123,16 +146,25 @@ def purge_numerical_data(database):
         b.append(hh)
     n = 1
     d = [b[i:i+n] for i in range(0, len(b), n)]
+    # put the number into a table
     db.executemany("insert into filter_seq_deco2 values (?)", (d))
     db.commit()
+    # select the unnique number in the filter seq deco2 table and insert to a new table
     db.execute("insert into filter_seq_deco3 select seq from filter_seq_deco2 group by seq having count (*) > 2 order by seq")
     db.commit()
     db.close()
     sucess = 'Done! '
     return sucess
 def break_to_aminoacid(database):
+    '''
+    This function was used to break the peptide sequence to single aminoacid.
+    This function will not be used in this script
+    :param database: the database you want to break to aminoacid
+    :return:Return a message said "Done!"
+    '''
     db = sqlite3.connect(database)
     cur = db.cursor()
+    # create two table that will used in the code
     try:
         cur.execute('DROP TABLE amino_acid')
         cur.execute('DROP TABLE amino_acid_pre')
@@ -158,12 +190,14 @@ def break_to_aminoacid(database):
             print('Database has been successfully created!')
         except:
             print('Database has already been created!')
+    # get whole table of peptide sequence
     seq_list = list(db.execute("select * from filter_seq"))
     a = []
     for i in range(len(seq_list)):
         split_seq = seq_list[i][0]
         for i in split_seq:
             a.append(i)
+    # put aminoacid sequence into the database
     db.executemany("insert into amino_acid_pre values (?)", (a))
     db.commit()
     db.execute("insert into amino_acid select seq from amino_acid_pre group by seq having count (*) > 2 order by seq")
@@ -175,17 +209,27 @@ def break_to_aminoacid(database):
 
 
 def fetch_data(database):
+    '''
+    This is the function that will seperate the raw data into different aminoacid table base on the aminoacid
+    the peptide sequence got.
+    This function will not be used in this script
+    :param database: This is the database you want to separate the raw data
+    :return: Return a message said "Done!"
+    '''
+    # this will allow the database to preform dirty read
     sqlite3.threadsafety = 2
     db = sqlite3.connect(database, isolation_level=None, check_same_thread=False)
     cur = db.cursor()
     db.execute("PRAGMA read_uncommitted = TRUE")
     db.execute("pragma journal_mode=wal")
+    # get basic information for the CPU
     cores = multiprocessing.cpu_count()
     pool = multiprocessing.Pool(processes=cores)
-    # lib = cdll.LoadLibrary("libdead_loop.so")
+    # get data in the aminoacid list
     seq_list = list(db.execute("select * from amino_acid"))
     sucess = 'Done! '
     long = len(seq_list)
+    # this is the multiprocessing part
     for i in range(long):
         sss = seq_list[i][0]
         pool.apply_async(sub_process(sss, db, cur), args=(sss, db, cur))
@@ -195,10 +239,18 @@ def fetch_data(database):
     return sucess
 
 def sub_process(seq_list, db, cur):
+    '''
+    This is the worker function for the previous function
+    :param seq_list: the sequence list
+    :param db: the database object
+    :param cur: the cursor object
+    :return: Return a message said "Done!"
+    '''
     numm = seq_list
     table = []
     table.append(numm)
     fin = ''.join(table)
+    # create table in the database
     try:
         cur.execute('DROP TABLE %s' % fin)
         # db.execute('VACUUM')
@@ -233,27 +285,28 @@ def sub_process(seq_list, db, cur):
             # print('Database has been successfully created!')
         except:
             print('ERROR in Creating database')
-    # print(seq_list[0][0])
+    # put data in the database
     db.execute("insert into " + fin + " select * from mgfData where seq like  '%" + seq_list + "%'")
     db.commit()
     sucess = 'Done! '
     return sucess
-# print(break_to_aminoacid(database))
 
-#
-# if __name__ == '__main__':
-#     print(fetch_data(database))
-#
-# print(generate_seq_decorate_list(database))
-# db = sqlite3.connect(database)
-# cur = db.cursor()
-# cur.execute('drop table filter_seq_deco')
 
-def get_50000(database, database2):
+def get_partial(database, database2, size):
+    '''
+    This is the function will select part data in the raw database and put it into a new database
+    (This is essential when the raw database is too large)
+    Tnis function will be called in this script
+    :param database: The database that contain the raw data
+    :param database2: the new database that will be created
+    :param size: the size you want to select out
+    :return: Return a message said "Done!"
+    '''
     db = sqlite3.connect(database)
     cur = db.cursor()
     db2 = sqlite3.connect(database2)
     cur2 = db2.cursor()
+    # This will created a table in the new database
     try:
         cur2.execute('DROP TABLE random')
         # db.execute('VACUUM')
@@ -288,9 +341,12 @@ def get_50000(database, database2):
             print('Database has been successfully created!')
         except:
             print('Database has already been created!')
+    # select the whole index of peptide sequence out and put it in a list
     full_list = list(db.execute('select * from filter_seq'))
-    short_list = random.sample(full_list, 5000)
+    # randomly choose size number of peptide sequence and put the corresponding data into the new database
+    short_list = random.sample(full_list, size)
     # print(short_list)
+    # put the data that select out to the new database
     for i in tqdm.tqdm(range(len(short_list))):
         a = list(db.execute('select * from mgfData where seq = ?', (short_list[i])))
         db2.executemany('insert into random values(?,?,?,?,?,?,?,?,?)', (a))
@@ -301,15 +357,14 @@ def get_50000(database, database2):
     success = "Done!"
     return success
 
-# print(purge_numerical_data(database2))
-# db = sqlite3.connect(database2)
-# db.execute('drop table filter_seq_deco')
-# db.execute('drop table filter_seq_deco2')
-# print(get_50000(database, database2))
-end = datetime.datetime.now()
-print(end - start)
 
 def clear_all(database):
+    '''
+    This is a function will clear the aminoacid table in the database
+    This function will not be called in this script
+    :param database: The database you want to clear
+    :return: no return
+    '''
     db = sqlite3.connect(database)
     cur = db.cursor()
     seq_list = list(db.execute("select * from amino_acid"))
@@ -324,8 +379,12 @@ def clear_all(database):
 # print(clear_all(database))
 
 if __name__ == "__main__":
-    database = 'test2.db'
-    # get_50000('test.db', 'test4.db')
-    # generate_seq_list('test4.db')
-    # generate_seq_decorate_list(database, 'random')
-    purge_numerical_data(database)
+    # original database is the database that contain the raw data
+    # the out database is the properly generated database that contain less data in it
+    original_database = 'test.db'
+    out_database = 'test6.db'
+    print(generate_seq_list(original_database, table_name='mgfData'))
+    print(get_partial(original_database, out_database, size=100))
+    print(generate_seq_list(out_database, table_name='random'))
+    print(generate_seq_decorate_list(out_database, table_name='random'))
+    # purge_numerical_data(database)
